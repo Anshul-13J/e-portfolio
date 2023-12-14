6a474e3f1aeb6983f9e39682b59a2273bbeaa3ad
Anshul-13J <74321084+Anshul-13J@users.noreply.github.com>
2023-12-14 20:34:16 +0530

    Updated e-Portfolio

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,44 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $subject = trim($_POST["subject"]);
+    $message = trim($_POST["message"]);
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'anshulnigam123@gmail.com';
+    // Validate data
+    if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+        echo "All fields are required!";
+        exit();
+    }
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    // You can add additional validation, such as email format validation
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    // Set the recipient email address
+    $to = "anshulnigam123@gmail.com";  // Replace with your email address
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    // Set the email subject
+    $email_subject = "New Contact Form Submission: $subject";
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    // Build the email message
+    $email_message = "Name: $name\n";
+    $email_message .= "Email: $email\n";
+    $email_message .= "Subject: $subject\n\n";
+    $email_message .= "Message:\n$message";
 
-  echo $contact->send();
+    // Set the email headers
+    $headers = "From: $email\n";
+    $headers .= "Reply-To: $email\n";
+
+    // Send the email
+    if (mail($to, $email_subject, $email_message, $headers)) {
+        echo "success";
+    } else {
+        echo "Error sending the message. Please try again later.";
+    }
+} else {
+    // If the request method is not POST, redirect to the form page or handle accordingly.
+    echo "Request method is not POST - Error from my side.."
+    exit();
+}
 ?>
